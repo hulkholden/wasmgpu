98c96bb2d2a36d4740e21e7f6066233446bb6eed
Paul Holden <paul@holden.dev>
2023-11-19 09:58:26 +0000

    Add CopyBufferToBuffer and ClearBuffer to GPUCommandBuffer.

--- command_encoder.go
+++ command_encoder.go
@@ -42,6 +42,36 @@ func (g GPUCommandEncoder) BeginComputePass(descriptor opt.T[GPUComputePassDescr
 	}
 }
 
+// CopyBufferToBuffer as described:
+// https://gpuweb.github.io/gpuweb/#dom-gpucommandencoder-copybuffertobuffer
+func (g GPUCommandEncoder) CopyBufferToBuffer(source GPUBuffer, sourceOffset GPUSize64, destination GPUBuffer, destinationOffset, size GPUSize64) {
+	params := make([]any, 5)
+	params[0] = source.ToJS()
+	params[1] = sourceOffset.ToJS()
+	params[2] = destination.ToJS()
+	params[3] = destinationOffset.ToJS()
+	params[4] = size.ToJS()
+	g.jsValue.Call("copyBufferToBuffer", params...)
+}
+
+// ClearBuffer as described:
+// https://gpuweb.github.io/gpuweb/#dom-gpucommandencoder-clearbuffer
+func (g GPUCommandEncoder) ClearBuffer(source GPUBuffer, offset, size GPUSize64) {
+	params := make([]any, 3)
+	params[0] = source.ToJS()
+	if offset > 0 {
+		params[1] = offset.ToJS()
+	} else {
+		params[1] = js.Undefined()
+	}
+	if size > 0 {
+		params[2] = size.ToJS()
+	} else {
+		params[2] = js.Undefined()
+	}
+	g.jsValue.Call("clearBuffer", params...)
+}
+
 // Finish as described:
 // https://gpuweb.github.io/gpuweb/#dom-gpucommandencoder-finish
 func (g GPUCommandEncoder) Finish() GPUCommandBuffer {
